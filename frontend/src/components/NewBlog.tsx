ffbfb46e58ec338755dc5da1d7de143b9703e83e
Yashita <yashita151293@gmail.com>
2024-04-20 14:54:57 +0530

    changed the shape of data sent by new blog page

--- frontend/src/components/NewBlog.tsx
+++ frontend/src/components/NewBlog.tsx
@@ -25,7 +25,10 @@ export const NewBlog = () => {
                 <Link to='/blogs'>
                     <button type="button" className="text-white bg-green-700 hover:bg-green-800  font-medium rounded-lg text-sm px-10 py-2.5" onClick={async()=>{
                         await axios.post(`${BACKEND_URL}/api/v1/blog`, {
-                           blogInputs
+                           blogInputs : {
+                            title : blogInputs.title,
+                            content : blogInputs.content
+                           }
                         },{
                             headers:{
                                 Authorization : localStorage.getItem('token')
